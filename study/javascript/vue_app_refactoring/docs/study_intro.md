08b29687fe15f6464da41a56427bbd0c54e721e4
sskim <amor061750@gmail.com>
2022-11-08 17:25:03 +0900

    vue-news 기본셋팅끝

---
+++ study/javascript/vue_app_refactoring/docs/study_intro.md
@@ -0,0 +1,18 @@
+### 공부할것
+
+1. 웹서비스 구현절차
+2. 컴포넌트 - 라우터 - API - 스토어 구현
+3. 컴포넌트 디자인패턴 + 비동기처리
+4. Mixins - HOC ( High Order Component )
+5. 외부라이브러리 모듈화, 실무프로젝트 구성
+6. 고급 라우터 패턴
+
+### 설계 방향 및 스타일
+- 스타일 문서 : [vue 공식문서](https://v2.vuejs.org/v2/style-guide/?redirect=true)
+- [해커뉴스](https://news.ycombinator.com/) 만들예정 : [API문서](https://github.com/tastejs/hacker-news-pwas/blob/master/docs/api.md)
+  
+- Router 설계
+  - News / Ask / Job 페이지[list]가 필요하니 이에 따른 Router
+  - New-list, Ask-list, Job-list -> 클릭한 아이템 정보
+  - 유저 정보를 보여주는 페이지
+
---
+++ study/javascript/vue_app_refactoring/my_source/vue-news/src/routes/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import AskView from '../views/AskView.vue'
+import JobsView from '../views/JobsView.vue'
+import NewView from '../views/NewsView.vue'
+import UserView from '../views/UserView.vue'
+import ItemView from '../views/ItemView.vue'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    mode: "history", // 이러면 # 라우터 없어짐
+    routes: [
+        { path: '/', redirect: '/news'          },
+        { path: '/news', component: NewView,    },
+        { path: '/ask', component: AskView,     },
+        { path: '/jobs', component: JobsView,   },
+        { path: '/user', component: UserView,   },
+        { path: '/item', component: ItemView,   },
+    ],
+});
