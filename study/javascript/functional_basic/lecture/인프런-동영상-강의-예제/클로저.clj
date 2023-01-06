77f8393ee12593069eb7129c44064d94b734785f
sskim <amor061750@gmail.com>
2023-01-06 13:07:24 +0900

    유인동 Func 기초4 - 실전1

--- study/javascript/functional_basic/lecture/인프런-동영상-강의-예제/클로저.clj
+++ study/javascript/functional_basic/lecture/인프런-동영상-강의-예제/클로저.clj
@@ -1,3 +1,5 @@
+(ns users.core)
+
 (def users [
   { :id 1 :name 'ID' :age 36 }
   { :id 2 :name 'BJ' :age 32 }
@@ -8,58 +10,60 @@
   { :id 7 :name 'JI' :age 31 }
   { :id 8 :name 'MP' :age 23 }])
 
-(println
- (map (fn [user] (:name user))
-      (filter (fn [user] (< (:age user) 30)) users)))
-;(PJ' HA' JE' MP')
-
-;  console.log(
-;    _map(user => user.age)
-;      (_filter(user => user.age < 30)(users)));
-
-
-(->> users
-     (filter (fn [user] (< (:age user) 30)))
-     (map (fn [user] (:name user)))
-     println)
-;(PJ' HA' JE' MP')
-
-;  _go(users,
-;    _filter(user => user.age < 30),
-;    _map(user => user.name),
-;    console.log);
-
-(->> users
-     (filter #(>= (:age %) 30))
-     (map #(:name %))
-     println)
-;(ID' BJ' JM' JI')
-
-(->> users
-     (filter (fn [user] (< (:age user) 30)))
-     (map (fn [user] (:age user)))
-     println)
-; (27 25 26 23)
-
-(->> users
-     (filter #(< (:age %) 30))
-     (map #(:age %))
-     (reduce +)
-     println)
-; 101
-
-
-; 병렬 처리
-(defn word-frequency1 [text]
-  (->>
-   (s/split text #"\s+")
-   (r/map s/lower-case)
-   (r/remove (fn [word] (s/starts-with? word "function")))
-   (r/map (fn [word] { word 1 }))
-   (r/fold (partial merge-with +))))
 
 (defn -main []
-  (time (word-frequency1 page))
-  (time (word-frequency1 page))
-  (time (word-frequency1 page))
-  (time (word-frequency1 page)))
+     (println
+          (map (fn [user] (:name user))
+               (filter (fn [user] (< (:age user) 30)) users)))
+     ;(PJ' HA' JE' MP')
+
+     ;  console.log(
+     ;    _map(user => user.age)
+     ;      (_filter(user => user.age < 30)(users)));
+
+
+     (->> users
+          (filter (fn [user] (< (:age user) 30)))
+          (map (fn [user] (:name user)))
+          println)
+     ;(PJ' HA' JE' MP')
+     ;  _go(users,
+     ;    _filter(user => user.age < 30),
+     ;    _map(user => user.name),
+     ;    console.log);
+
+     (->> users
+          (filter #(>= (:age %) 30))
+          (map #(:name %))
+          println)
+     ;(ID' BJ' JM' JI')
+
+     (->> users
+          (filter (fn [user] (< (:age user) 30)))
+          (map (fn [user] (:age user)))
+          println)
+     ; (27 25 26 23)
+
+     (->> users
+          (filter #(< (:age %) 30))
+          (map #(:age %))
+          (reduce +)
+          println)
+     ; 101
+
+
+     ; 병렬 처리
+     (defn word-frequency1 [text]
+     (->>
+     (s/split text #"\s+")
+     (r/map s/lower-case)
+     (r/remove (fn [word] (s/starts-with? word "function")))
+     (r/map (fn [word] { word 1 }))
+     (r/fold (partial merge-with +))))
+
+     (defn -main []
+     (time (word-frequency1 page))
+     (time (word-frequency1 page))
+     (time (word-frequency1 page))
+     (time (word-frequency1 page)))
+)
